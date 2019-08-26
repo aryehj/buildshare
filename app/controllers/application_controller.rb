b1928e7cdbae2957664a5ae61309bdf856fd897d
Aryeh Jacobsohn <aryehj@gmail.com>
2019-08-26 10:21:21 -0500

    Create welcome page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token, raise: false
+  
+  def welcome
+    render{ :plain => "Aryeh wuz here." }
+  end 
+  
 end
