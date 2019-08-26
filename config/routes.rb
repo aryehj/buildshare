4a1e6e19e199e75af900eb80cc5aa87f1da3bd37
Aryeh Jacobsohn <aryehj@gmail.com>
2019-08-26 10:20:18 -0500

    Create route for welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-
+match("/", {
+    :controller => "application", 
+    :action => "welcome",
+    :via => "get"
+           }
+     )
+  
   # ==============
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
