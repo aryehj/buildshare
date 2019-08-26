915a4bff45bc8b7812bdc5679bd7af811475f902
Aryeh Jacobsohn <aryehj@gmail.com>
2019-08-26 10:23:30 -0500

    Correct render syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token, raise: false
   
   def welcome
-    render{ :plain => "Aryeh wuz here." }
+    render({ :plain => "Aryeh wuz here." })
   end 
   
 end
