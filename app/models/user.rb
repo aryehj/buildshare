80b4bc8bd25193614e80e85418a80f9269c52915
aryehj <aryehj@gmail.com>
2019-08-28 12:05:57 -0500

    add email to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
 #  name          :string
+#  email         :string
 #
 
 class User < ApplicationRecord
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190828170433) do
+ActiveRecord::Schema.define(version: 20190828170536) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 20190828170433) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "email"
   end
 
   create_table "votes", force: :cascade do |t|
