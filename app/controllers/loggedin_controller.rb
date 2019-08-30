24bb6596391285d2020530bf2dbc20a961fd2422
Aryeh Jacobsohn <aryehj@gmail.com>
2019-08-29 23:12:55 -0500

    create controller for auth-d stuff

---
+++ app/controllers/loggedin_controller.rb
@@ -0,0 +1,15 @@
+class LoggedInController < ApplicationController
+
+before_action :check_auth
+
+def check_auth
+  if session[:user_id].present?
+    return true
+  else
+    redirect_to("/sign_in",
+      { :alert => "You must be signed in to see that page." }
+    )
+  end
+end
+
+end
