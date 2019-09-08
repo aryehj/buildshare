9a4108da1ecadc434ff505233604bc74afb25e8b
Aryeh Jacobsohn <aryehj@gmail.com>
2019-09-08 11:52:19 -0500

    remove heroku-killing migrations

--- db/migrate/20190908153925_change_column_name.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnName < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :proposals, :owned_by_user_id, :owner_id
-    rename_column :steps, :volunteer_user_id, :volunteer_id
-  end
-end
